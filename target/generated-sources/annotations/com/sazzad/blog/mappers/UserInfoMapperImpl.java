a13e6d5e3d6411355f606ec9618b5012485d3a64
EyepatchWizard <shsazzadhossainy@gmail.com>
2025-08-16 09:49:00 +0600

    changes the DB from render to supabase

--- target/generated-sources/annotations/com/sazzad/blog/mappers/UserInfoMapperImpl.java
+++ target/generated-sources/annotations/com/sazzad/blog/mappers/UserInfoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-15T20:02:56+0600",
+    date = "2025-08-16T09:45:33+0600",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
